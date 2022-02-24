ab39850c05fbe42e39d50b68eb0bad8d803556f9
Bryan Albert <bryanalbert@hotmail.com>
2022-02-24 12:41:13 -0800

    Refactor console application, handle JoinChat and LeaveChat messages.

--- SignalRConsole/Program.cs
+++ SignalRConsole/Program.cs
@@ -4,39 +4,45 @@ using System.Threading.Tasks;
 
 namespace SignalRConsole
 {
-    class Program
-    {
-        private static HubConnection hubConnection;
+	class Program
+	{
+		private static async Task Main(string[] args)
+		{
+			if (args is null)
+				throw new ArgumentNullException(nameof(args));
 
-        static void Main(string[] args)
-        {
-            hubConnection = new HubConnectionBuilder().WithUrl("https://localhost:44398/chathub").Build();
+			Console.WriteLine($"Connecting to URL: {c_chatHubUrl}");
 
-            hubConnection.StartAsync().Wait();
+			Console.Write("What is your name? ");
+			string name = Console.ReadLine();
 
-            hubConnection.On<string, string>("ReceiveMessage", (user, receivedMessage) =>
-            {
-                Console.WriteLine($"{user} said {receivedMessage}");
-            });
+			HubConnection hubConnection = new HubConnectionBuilder().WithUrl(c_chatHubUrl).Build();
+			
+			_ = hubConnection.On<string>("JoinChat", (user) => Console.WriteLine($"{user} has joined the chat."));
+			hubConnection.StartAsync().Wait();
+			_ = hubConnection.On<string, string>("ReceiveMessage", (user, receivedMessage) =>
+				Console.WriteLine($"{user} said: {receivedMessage}"));
+			_ = hubConnection.On<string>("LeaveChat", (user) => Console.WriteLine($"{user} has left the chat."));
+			
+			await hubConnection.SendAsync("JoinChat", name);
+
+			Console.WriteLine("\nType messages, type 'goodbye' to leave the chat.");
+			string message;
+			do
+			{
+				message = Console.ReadLine();
+				hubConnection.SendAsync("SendMessage", name, message).Wait();
+			}
+			while (message != "goodbye");
+
+			hubConnection.SendAsync("LeaveChat", name).Wait();
+		}
 
 
-            Console.Write("Type any message: ");
-            var keepGoing = true;
-            do
-            {
-                var text = Console.ReadLine();
-                if (text == "exit")
-                {
-                    keepGoing = false;
-                }
-                else
-                {
-                    hubConnection.SendAsync("SendMessage", "SampleUser", text).Wait();
-                }
-            }
-            while (keepGoing);
-
-            Console.ReadKey();
-        }
-    }
+#if true
+		private static readonly string c_chatHubUrl = "https://localhost:5001/chathub";
+#else
+		private static readonly string c_chatHubUrl = "https://localhost:44398/chathub";
+#endif
+	}
 }
