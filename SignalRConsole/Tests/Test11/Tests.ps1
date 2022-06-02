d970e0d7c976a3426abebc0ae0889ca514d012b7
Bryan Albert <bryanalbert@hotmail.com>
2022-06-02 15:45:35 -0700

    Add tests 9 through 11.

---
+++ SignalRConsole/Tests/Test11/Tests.ps1
@@ -0,0 +1,71 @@
+# Bruce blocked by Fred, adds Fred 
+
+$Global:tests = "Test11"
+
+function Reset-Test
+{
+	"Resetting $tests"
+	Push-Location $tests
+	Copy-Item .\BruceFriends.qkr .\Bruce.qkr.json
+	if (Test-Path .\BruceOutput.txt) { Remove-Item .\BruceOutput.txt }
+	Pop-Location
+}
+
+function Run-Test
+{
+	$script = Join-Path $tests "Test.txt"
+	"Running script $script"
+	dotnet.exe .\SignalRConsole.dll $script
+	Push-Location $tests
+	$global:warningCount = 0
+	$global:errorCount = 0
+	Compare-Files .\BruceControl.txt .\BruceOutput.txt $true
+	Compare-Files .\BruceControl.qkr .\Bruce.qkr.json $false
+
+	"Total warning count: $global:warningCount"
+	"Total error count: $global:errorCount"
+	Pop-Location
+}
+
+function Print-Files
+{
+	"Results for $tests"
+	Push-Location $tests
+	Get-ChildItem *.qkr.json | ForEach-Object { $_.Name; Get-Content $_; "" }
+	Get-ChildItem *Output.txt | ForEach-Object { $_.Name; Get-Content $_; "" }
+	Pop-Location
+}
+
+function Update-ControlFiles
+{
+	"Updating control files for $tests"
+	Push-Location $tests
+	Copy-Item .\BruceOutput.txt .\BruceControl.txt
+	Copy-Item .\Bruce.qkr.json .\BruceControl.qkr
+	Pop-Location
+}
+
+function Update-SignalRConsole
+{
+	Get-ChildItem ..\bin\Debug\netcoreapp3.1\* -File | Copy-Item -Destination .
+}
+
+
+function Compare-Files($control, $file, $logFile)
+{
+	"Comparing: $control with $file"
+	if (((Compare-Object (Get-Content $control) (Get-Content $file)) | Measure-Object).Count -gt 0) {
+		if ($logFile)
+		{
+			"Warning: $file has unexpected output:"
+			$global:warningCount++
+		}
+		else
+		{
+			"Error: $file has unexpected output:"
+			$global:errorCount++
+		}
+
+		Compare-Object (Get-Content $control) (Get-Content $file)
+	}
+}
--- SignalRConsole/Tests/Tests.ps1
+++ SignalRConsole/Tests/Tests.ps1
@@ -1,17 +1,32 @@
-$global:totalErrorCount = 0
-
-function Run-Tests
+function Reset-Tests
 {
-    $global:totalErrorCount = 0
     $testCount = 0
     Get-ChildItem Test* -Directory | ForEach-Object {
         $testCount++
         . (Join-Path $_ Tests.ps1)
-        "`nRunning tests fromn: $tests"
+        "`nResetting test fromn: $tests"
+        Reset-Test
+    }
+
+    "Reset $testCount tests."
+}
+
+function Run-Tests
+{
+    $global:totalWarningCount = 0
+    $global:totalErrorCount = 0
+    $testCount = 0
+    Get-ChildItem Test* -Directory | ForEach-Object
+    {
+        $testCount++
+        . (Join-Path $_ Tests.ps1)
+        "`nRunning test fromn: $tests"
         Reset-Test
         Run-Test
+        $global:totalWarningCount += $global:warningCount
         $global:totalErrorCount += $global:errorCount
     }
 
-    "`nTotal error count across $testCount tests: $global:totalErrorCount"
+    "`nTotal warning count across $testCount tests: $global:totalWarningCount"
+    "Total error count across $testCount tests: $global:totalErrorCount"
 }
