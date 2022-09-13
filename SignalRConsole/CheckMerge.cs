41d46f48620ad5967225f1bcdba346bdfd50c951
Bryan Albert <bryanalbert@hotmail.com>
2022-09-13 11:45:32 -0700

    Merge check supports more than two devices.

--- SignalRConsole/CheckMerge.cs
+++ SignalRConsole/CheckMerge.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Text.Json;
 
 namespace SignalRConsole
@@ -19,6 +17,7 @@ namespace SignalRConsole
 		public string Folder { get; }
 		public string Handle { get; }
 		public List<User[]> Users { get; } = new List<User[]>();
+		public List<string> Devices { get; } = new List<string>();
 
 		internal bool RunCheck()
 		{
@@ -31,9 +30,12 @@ namespace SignalRConsole
 				{
 					foreach (Card card in table.Cards)
 					{
-						int totalQuizzed = card.MergeQuizzed[0];
-						int totalCorrect = card.MergeCorrect[0];
-						int totalTime = card.MergeTime[0];
+						Dictionary<string, int> quizzed = new Dictionary<string, int>();
+						Dictionary<string, int> correct = new Dictionary<string, int>();
+						Dictionary<string, int> time = new Dictionary<string, int>();
+						GetQuizzed(Users[0][1], kind, card, quizzed);
+						GetCorrect(Users[0][1], kind, card, correct);
+						GetTime(Users[0][1], kind, card, time);
 
 						for (int index = 1; index < Users.Count; index++)
 						{
@@ -41,16 +43,22 @@ namespace SignalRConsole
 								Tables.FirstOrDefault(x => x.Base == table.Base).Cards.FirstOrDefault(x =>
 								x.Fact.First == card.Fact.First && x.Fact.Second == card.Fact.Second);
 
-							if (!CheckCardTotals(index, kind, card, match))
+							if (!CheckCardTotals(Users[index][1], kind, card, match))
 								return false;
 
-							totalQuizzed += match.MergeQuizzed[0];
-							totalCorrect += match.MergeCorrect[0];
-							totalTime += match.MergeTime[0];
+							if (!GetQuizzed(Users[index][1], kind, card: match, quizzed: quizzed) ||
+								!GetCorrect(Users[index][1], kind, match, correct) ||
+								!GetTime(Users[index][1], kind, match, time))
+							{
+								return false;
+							}
 						}
 
-						if (!CheckTotals(kind, card, totalQuizzed, totalCorrect, totalTime))
+						if (!CheckTotals(kind, card, quizzed.Sum(x => x.Value), correct.Sum(x => x.Value),
+							time.Sum(x => x.Value)))
+						{
 							return false;
+						}
 					}
 				}
 			}
@@ -58,11 +66,80 @@ namespace SignalRConsole
 			return true;
 		}
 
-		private bool CheckCardTotals(int index, OperatorTables kind, Card card, Card match)
+		private bool GetQuizzed(User user, OperatorTables kind, Card card, Dictionary<string, int> quizzed)
+		{
+			for (int index = 0; index < card.MergeQuizzed.Length; index++)
+			{
+				string device = user.MergeIndex.FirstOrDefault(x => x.Value == index).Key;
+				if (!quizzed.ContainsKey(device))
+				{
+					quizzed[device] = card.MergeQuizzed[index];
+					continue;
+				}
+
+				if (quizzed[device] != card.MergeQuizzed[index])
+				{
+					Console.WriteLine($"Error: MergeQuizzed mismatch for {device} in {user.FileName} for {kind.Name}," +
+						$" card {card.Fact.First} by {card.Fact.Second}, should be {quizzed[device]} not {card.MergeQuizzed[index]}");
+	
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		private bool GetCorrect(User user, OperatorTables kind, Card card, Dictionary<string, int> correct)
+		{
+			for (int index = 0; index < card.MergeCorrect.Length; index++)
+			{
+				string device = user.MergeIndex.FirstOrDefault(x => x.Value == index).Key;
+				if (!correct.ContainsKey(device))
+				{
+					correct[device] = card.MergeCorrect[index];
+					continue;
+				}
+
+				if (correct[device] != card.MergeCorrect[index])
+				{
+					Console.WriteLine($"Error: MergeCorrect mismatch for {device} in {user.FileName} for {kind.Name}," +
+						$" card {card.Fact.First} by {card.Fact.Second}, should be {correct[device]} not {card.MergeCorrect[index]}");
+
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		private bool GetTime(User user, OperatorTables kind, Card card, Dictionary<string, int> time)
+		{
+			for (int index = 0; index < card.MergeTime.Length; index++)
+			{
+				string device = user.MergeIndex.FirstOrDefault(x => x.Value == index).Key;
+				if (!time.ContainsKey(device))
+				{
+					time[device] = card.MergeTime[index];
+					continue;
+				}
+
+				if (time[device] != card.MergeTime[index])
+				{
+					Console.WriteLine($"Error: MergeTime mismatch for {device} in {user.FileName} for {kind.Name}," +
+						$" card {card.Fact.First} by {card.Fact.Second}, should be {time[device]} not {card.MergeTime[index]}");
+
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		private bool CheckCardTotals(User user, OperatorTables kind, Card card, Card match)
 		{
 			if (match.Quizzed != card.Quizzed)
 			{
-				Console.WriteLine($"Error: Quizzed mismatch in {Users[index][1].FileName} for {kind.Name}," +
+				Console.WriteLine($"Error: Quizzed mismatch in {user.FileName} for {kind.Name}," +
 					$" card {match.Fact.First} by {match.Fact.Second}, Quizzed is {match.Quizzed} not {card.Quizzed}");
 
 				return false;
@@ -70,7 +147,7 @@ namespace SignalRConsole
 
 			if (match.Correct != card.Correct)
 			{
-				Console.WriteLine($"Error: Correct mismatch in {Users[index][1].FileName} for {kind.Name}," +
+				Console.WriteLine($"Error: Correct mismatch in {user.FileName} for {kind.Name}," +
 					$" card {match.Fact.First} by {match.Fact.Second}, Correct is {match.Correct} not {card.Correct}");
 
 				return false;
@@ -78,7 +155,7 @@ namespace SignalRConsole
 
 			if (match.TotalTime != card.TotalTime)
 			{
-				Console.WriteLine($"Error: TotalTime mismatch in {Users[index][1].FileName} for {kind.Name}," +
+				Console.WriteLine($"Error: TotalTime mismatch in {user.FileName} for {kind.Name}," +
 					$" card {match.Fact.First} by {match.Fact.Second}, Quizzed is {match.TotalTime} not {card.TotalTime}");
 
 				return false;
@@ -87,27 +164,27 @@ namespace SignalRConsole
 			return true;
 		}
 
-		private bool CheckTotals(OperatorTables kind, Card card, int totalQuizzed, int totalCorrect, int totalTime)
+		private bool CheckTotals(OperatorTables kind, Card card, int quizzed, int correct, int time)
 		{
-			if (card.Quizzed != totalQuizzed)
+			if (card.Quizzed != quizzed)
 			{
-				Console.WriteLine($"Error: MergeQuizzed total {totalQuizzed} does not match {kind.Name} card" +
+				Console.WriteLine($"Error: MergeQuizzed total {quizzed} does not match {kind.Name} card" +
 					$" {card.Fact.First} by {card.Fact.Second} Quizzed {card.Quizzed}.");
 
 				return false;
 			}
 
-			if (card.Correct != totalCorrect)
+			if (card.Correct != correct)
 			{
-				Console.WriteLine($"Error: MergeCorrect total {totalCorrect} does not match {kind.Name} card" +
+				Console.WriteLine($"Error: MergeCorrect total {correct} does not match {kind.Name} card" +
 					$" {card.Fact.First} by {card.Fact.Second} Correct {card.Correct}.");
 
 				return false;
 			}
 
-			if (card.TotalTime != totalTime)
+			if (card.TotalTime != time)
 			{
-				Console.WriteLine($"Error: MergeTotalTime total {totalTime} does not match {kind.Name} card" +
+				Console.WriteLine($"Error: MergeTotalTime total {time} does not match {kind.Name} card" +
 					$" {card.Fact.First} by {card.Fact.Second} TotalTime {card.TotalTime}.");
 
 				return false;
@@ -134,7 +211,9 @@ namespace SignalRConsole
 					return false;
 				}
 
+				user[0].FileName = path;
 				user[1].FileName = path + ".json";
+				Devices.Add(user[1].DeviceId);
 
 				Users.Add(user);
 			}
